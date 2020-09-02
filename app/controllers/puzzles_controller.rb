63121b94a077c37c359ec261eb509698ae1ca858
Nick Schrock <lnhschrock@gmail.com>
2020-09-01 22:20:03 -0400

    update auth

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,6 +1,6 @@
 class PuzzlesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "admin", password: "granitespoon", except: [:index, :show]
 
   def index
     @puzzles = Puzzle.all
