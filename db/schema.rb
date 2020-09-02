cf9c9365d0e5474288a5f601881de64ff76648b5
Nick Schrock <lnhschrock@gmail.com>
2020-09-01 21:18:47 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_30_010522) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "puzzles", force: :cascade do |t|
     t.string "name"
     t.text "game"
